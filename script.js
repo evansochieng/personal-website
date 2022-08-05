98a9a96da4c519034c4ec133de5245dea9efe832
Evans Ochieng <evansochieng121@gmail.com>
2022-08-05 16:54:28 +0300

    Add basic website content

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ about.textContent = 'A confident and determined young man with entry\
                enjoy coding, have no problem in continuously learning,\
                adaptive to high stress environments and stay calm under \
                pressure. I\'m a believer in solving problems using \
-               mathematics and codes. \n Currently learning Software \
+               mathematics and codes. \nCurrently learning Software \
                Development to integrate these skills in building\
                interactive user applications for my data science projects\
                .';
